0dbc0db595502b3084a8139e0b2e3ee88135a73e
Andres Oliva <andres.oliva@teufel.de>
2023-03-27 17:51:09 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+#![no_std]
+#![no_main]
+#![feature(type_alias_impl_trait)]
+
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_time::{Duration, Timer};
+use {defmt_rtt as _, panic_probe as _};
+
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let _p = embassy_stm32::init(Default::default());
+    info!("Hello World!");
+
+    loop {
+        info!("high");
+        Timer::after(Duration::from_millis(300)).await;
+
+        info!("low");
+        Timer::after(Duration::from_millis(300)).await;
+    }
+}
